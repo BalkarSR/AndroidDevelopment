eeccbe2cdd338623f587488b60cb7e42e490c47d
Balkar Rana <balkar_rana@hotmail.com>
2018-05-22 13:37:09 -0400

    final

--- Lab2/app/src/main/java/com/example/balkarrana/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/balkarrana/lab2/MainActivity.java
@@ -7,43 +7,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    Button userButton, passButton, userPassButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button userButton = findViewById(R.id.userButton);
-        userButton.setOnClickListener(this);
+        final EditText userText = findViewById(R.id.username), passText = findViewById(R.id.password);
+        final Button userButton = findViewById(R.id.userButton);
+        final Button passButton = findViewById(R.id.passButton);
+        final Button userPassButton = findViewById(R.id.userPassButton);
 
-        Button passButton = findViewById(R.id.passButton);
-        passButton.setOnClickListener(this);
-
-        Button userPassButton = findViewById(R.id.userPassButton);
-        userPassButton.setOnClickListener(this);
-
-    }
-
-    public void onCreate(View v){
-
-        EditText userText = findViewById(R.id.username), passText = findViewById(R.id.password);
-
-        switch(v.getId()){
-
-            case R.id.userButton:
+        userButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
                 userButton.setText(userText.getText().toString());
-                break;
-            case R.id.passButton:
+            }
+
+        });
+
+        passButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
                 passButton.setText(passText.getText().toString());
-                break;
-            case R.id.userPassButton:
-                String userPass = userText.getText().toString() + "-" + passText.getText().toString();
-                userPassButton.setText(userPass);
-                break;
-            default:
-        }
+            }
+
+        });
+
+        userPassButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                userPassButton.setText(userText.getText().toString() + "-" + passText.getText().toString());
+            }
+
+        });
 
     }
 
